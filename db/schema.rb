737fcf81c6f83dcdc3e73653cbca587d37e00ef4
Jackie Chui <jshchui@gmail.com>
2017-07-29 14:25:07 -0700

    initializing nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170729181138) do
-
+ActiveRecord::Schema.define(version: 20170729201700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
