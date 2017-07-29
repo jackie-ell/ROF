680823183cc7e4cecfc522915d90b59bc2e376b4
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2017-07-29 15:02:20 -0700

    add creategroup

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
-  belongs_to :question
-  belongs_to :user
+  belongs_to :question, optional: true
+  belongs_to :user, optional: true
 
   validates :body, presence: true
 end
