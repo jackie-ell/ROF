efbb90b3dec5e8931ab53227b85db55df8518069
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-07-29 12:05:37 -0700

    Add question controller; all actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,58 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_drill_group, only: [:create]
+  before_action :find_question, only: [:destroy]
+  before_action :authorize_user!, except: [:index, :show]
+
   def index
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new question_params
+    @question.drill_group = @drill_group
+    @question.user = current_user
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new, alert: @question.errors.full_messages.join(', ')
+    end
   end
 
   def show
+    @questions = Question.all
   end
 
   def destroy
+    if @question.destroy
+      redirect_to @question.drill_group
+    else
+      redirect_to @question, alert: @question.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:description, :points, :title)
+  end
+
+  def find_drill_group
+    @drill_group = DrillGroup.find(params[:drill_group_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def authorize_user!
+    unless can?(:manage, @question)
+      redirect_to root_path, alert: 'Unauthorized access.'
+
+      head :unauthorized
+    end
   end
 end
