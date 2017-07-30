abfd00cebdd96fc0c7f6116115feea0706a9db81
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-07-30 11:59:37 -0700

    Add users_question to question controller, Add #update action to users_questions controller, update users_question route, update question show page

--- app/controllers/users_questions_controller.rb
+++ app/controllers/users_questions_controller.rb
@@ -1,6 +1,7 @@
 class UsersQuestionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_question, only: [:create]
+  before_action :find_question
+  before_action :find_users_question, only: [:update]
 
   def create
     @users_question = UsersQuestion.new
@@ -25,15 +26,32 @@ class UsersQuestionsController < ApplicationController
   end
 
   def update
-    
+    @users_question.num_attempts += 1
+    @users_question.last_answer = params[:body]
+
+    if @users_question.question.answers.find_by(body: @users_question.last_answer)
+      @users_question.is_correct = true
+    else
+      @users_question.is_correct = false
+    end
+
+    if @users_question.save
+      redirect_to @question
+    else
+      redirect_to @question, alert: @users_question.errors.full_messages.join(', ')
+    end
   end
 
   private
-  def users_questions_params
-    params.require(:users_question).permit(:body)
-  end
+  # def users_questions_params
+  #   params.require(:users_question).permit(:body)
+  # end
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def find_users_question
+    @users_question = UsersQuestion.find_by(user: current_user, question: params[:question_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :questions, only: [:new, :create, :destroy, :index, :show] do
-    resources :users_questions, only: [:new, :create, :destroy, :index]
+    resources :users_questions, only: [:create, :update]
   end
 
 
