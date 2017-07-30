2cff2a6b1a51c94ea9390d362cd7f39bade763de
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-07-30 11:38:29 -0700

    Create #add action to users_questions controller, modified question show.html to display is_correct

--- app/controllers/users_questions_controller.rb
+++ app/controllers/users_questions_controller.rb
@@ -1,14 +1,39 @@
 class UsersQuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_question, only: [:create]
+
   def create
     @users_question = UsersQuestion.new
+    @users_question.user = current_user
+    @users_question.question = @question
+    @users_question.num_attempts = 1
+    @users_question.last_answer = params[:body]
+
+    # Check for correct answer
+    if @users_question.question.answers.find_by(body: @users_question.last_answer)
+      @users_question.is_correct = true
+    else
+      @users_question.is_correct = false
+    end
+
+    if @users_question.save
+      redirect_to @question
+    else
+      redirect_to @question, alert: @users_question.errors.full_messages.join(', ')
+    end
+
   end
 
   def update
-
+    
   end
 
   private
   def users_questions_params
+    params.require(:users_question).permit(:body)
+  end
 
+  def find_question
+    @question = Question.find(params[:question_id])
   end
 end
