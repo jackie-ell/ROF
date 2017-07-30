9fe781c89d51affa78f1622a76a9e70fe0b8c509
Colin Nebocat <colin@nebocat.ca>
2017-07-30 11:41:01 -0700

    Hide signup form when user signed in and add placeholder text to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
   end
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,7 @@ class Ability
     end
 
 
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
