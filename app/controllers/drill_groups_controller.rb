5e92df4dcccb533f6b1c9732f3e667d064c5124f
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-07-30 16:39:20 -0700

    Drill percent (#57)
    * Add percent_complete function and displayed it on index.html
    
    * Fix percent calculation

--- app/controllers/drill_groups_controller.rb
+++ app/controllers/drill_groups_controller.rb
@@ -9,7 +9,7 @@ class DrillGroupsController < ApplicationController
   end
 
   def create
-    
+
     drill_group = params.require(:drill_group).permit(:category,  :description, :difficulty)
    if  DrillGroup.create(drill_group)
      redirect_to drill_groups_path, notice: 'drill_group created!'
@@ -23,5 +23,12 @@ class DrillGroupsController < ApplicationController
     @questions = Question.where(drill_group_id: params[:id])
   end
 
+  def percent_complete(dg, user)
+    uq = UsersQuestion.where(user: user)
+    correct = uq.where(user: user, is_correct: true, question: dg.questions).count
 
+    # return Correct answers / Total questions
+    return ((correct.to_f / dg.questions.count)*100).round(1)
+  end
+  helper_method :percent_complete
 end
