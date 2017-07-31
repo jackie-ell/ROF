5a60a55012f8cfda3491a852d3c24777dc945989
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2017-07-30 19:02:20 -0700

    add button

--- app/controllers/drill_groups_controller.rb
+++ app/controllers/drill_groups_controller.rb
@@ -1,6 +1,6 @@
 class DrillGroupsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_drill_group, only: [:create, :destroy, :update]
+  before_action :find_drill_group, only: [:destroy, :update]
   before_action :authorize_user!, only: [:new, :create, :destroy, :update]
 
   def index
@@ -15,8 +15,8 @@ class DrillGroupsController < ApplicationController
 
     @drill_group = params.require(:drill_group).permit(:category,  :description, :difficulty)
    if  DrillGroup.create(@drill_group)
-
-     redirect_to drill_groups_path, notice: 'drill_group created!'
+       drillgroup_id = DrillGroup.last.id
+       redirect_to new_drill_group_question_path(drillgroup_id), notice: 'drill_group created!'
    end
   end
 
